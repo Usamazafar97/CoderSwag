3f433e405979a01a6be004b35dac0a7377f780df
usamazafar <osamazafar97@gmail.com>
2022-04-05 21:01:01 +0500

    Code completed product activity added recycler view added view holder added custom list view - completed default list view introduced

--- app/src/main/java/com/example/coderswag/controller/ProductsActivity.kt
+++ app/src/main/java/com/example/coderswag/controller/ProductsActivity.kt
@@ -1,12 +1,21 @@
 package com.example.coderswag.controller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.coderswag.R
+import com.example.coderswag.adapters.ProductAdapter
+import com.example.coderswag.services.DataService
 import com.example.coderswag.utilities.EXTRA_PRODUCT
+import kotlinx.android.synthetic.main.activity_products.*
+
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
@@ -15,6 +24,13 @@ class ProductsActivity : AppCompatActivity() {
 
         println(productType)
         Toast.makeText(this, "Product type: $productType", Toast.LENGTH_SHORT).show()
+
+        //
+        adapter = ProductAdapter(this, DataService.getProductList(productType))
+
+        productsListView.adapter = adapter
+        productsListView.layoutManager = GridLayoutManager(this, 2)
+
 
     }
 }
